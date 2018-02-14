2fdbca6e9fda41e30a76faee5f45ad1c8d43913f
jordan3900 <m.iordanize@gmail.com>
2018-02-14 13:08:58 +0200

    Ex

---
+++ DefiningClasses-Exercise/03.OldestFamilyMember/StartUp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+class StartUp
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        var familyMembers = new List<Person>();
+        for (int i = 0; i < n; i++)
+        {
+            var tokens = Console.ReadLine().Split();
+            var age = int.Parse(tokens[1]);
+            var name = tokens[0];
+            var member = new Person(name,age);
+            familyMembers.Add(member);
+        }
+        var oldestMember = familyMembers.OrderByDescending(a => a.Age).First();
+        Console.WriteLine($"{oldestMember.Name} {oldestMember.Age}");
+    }
+}
+
