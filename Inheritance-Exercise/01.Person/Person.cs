40c15b0b303b2e01eff6abff05c4a6528a546943
jordan3900 <m.iordanize@gmail.com>
2018-02-25 19:52:35 +0200

    Ex

---
+++ Inheritance-Exercise/01.Person/Person.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+
+public class Person
+{
+    private string name;
+    private int age;
+
+    public string Name
+    {
+        get => this.name;
+        set
+        {
+            if (value.Length < 3)
+            {
+                throw new ArgumentException("Name's lenght should not be less than 3 symbols!");
+            }
+            this.name = value;
+        }
+    }
+
+    public int Age
+    {
+        get => this.age;
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Age must be positive!");
+            }
+            this.age = value;
+        }
+    }
+    public Person(string name, int age)
+    {
+        this.Name = name;
+        this.Age = age;
+    }
+}
+
+
