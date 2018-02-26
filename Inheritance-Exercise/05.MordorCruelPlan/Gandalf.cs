abc863cd8c766166b3ae2b18a46a8c4924a0a9ea
jordan3900 <m.iordanize@gmail.com>
2018-02-26 18:40:36 +0200

    Exercise

---
+++ Inheritance-Exercise/05.MordorCruelPlan/Gandalf.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class Gandalf
+{
+    private int hapinesPoint;
+
+    public Gandalf()
+    {
+        this.hapinesPoint = 0;
+    }
+
+    public int HapinesPoint => this.hapinesPoint;
+
+    public void EatingFood(string food)
+    {
+        var foodPoint = new Dictionary<string, int>();
+
+     
+        foodPoint["cram"] = 2;
+        foodPoint["lembas"] = 3;
+        foodPoint["apple"] = 1;
+        foodPoint["melon"] = 1;
+        foodPoint["honeycake"] = 5;
+        foodPoint["mushrooms"] = -10;
+
+        if (foodPoint.ContainsKey(food))
+        {
+            this.hapinesPoint += foodPoint[food];
+        }
+        else
+        {
+            this.hapinesPoint -= 1;
+        }
+    }
+
+    public void GetTheMood()
+    {
+        if (hapinesPoint < -5)
+        {
+            Console.WriteLine("Angry");
+        }
+        else if (hapinesPoint == -5 || hapinesPoint <= 0)
+        {
+            Console.WriteLine("Sad");
+        }
+        else if (hapinesPoint > 15)
+        {
+            Console.WriteLine("JavaScript");
+        }
+        else if (hapinesPoint > 0 || hapinesPoint <= 15)
+        {
+            Console.WriteLine("Happy");
+        }
+       
+    }
+}
+
---
+++ Inheritance-Exercise/05.MordorCruelPlan/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class StartUp
+{
+    static void Main(string[] args)
+    {
+        var foodTokens = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+        var gandalf = new Gandalf();
+        foreach (var food in foodTokens)
+        {
+            gandalf.EatingFood(food.ToLower());
+        }
+        Console.WriteLine(gandalf.HapinesPoint);
+        gandalf.GetTheMood();
+    }
+}
+
