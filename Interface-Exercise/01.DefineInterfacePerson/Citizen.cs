f47546f44d858c46b37b3d936d3b889ad200d444
jordan3900 <m.iordanize@gmail.com>
2018-02-27 18:17:18 +0200

    Exercise

---
+++ Interface-Exercise/01.DefineInterfacePerson/Citizen.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+
+public class Citizen : IPerson
+{
+    private string name;
+    private int age;
+
+    public Citizen(string name, int age)
+    {
+        this.Name = name;
+        this.Age = age;
+    }
+
+    public string Name { get; set; }
+    public int Age { get; set; }
+}
+
